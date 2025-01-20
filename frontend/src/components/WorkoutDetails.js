62d6aa81980a74c1c33d557c10bd559f11d53308
PWINT KO OO <23029589@myrp.edu.sg>
2025-01-20 14:07:42 +0800

    update

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -6,7 +6,7 @@ function WorkoutDetails({workout}) {
     const {dispatch} = useWorkoutsContext();
 
     const handleClick = async() => {
-        const response = await fetch('/api/workouts/'+workout._id, {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workouts/${workout.id}`, {
             method: 'DELETE'
         });
         const json = await response.json();
--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -14,7 +14,9 @@ function WorkoutForm() {
 
         const workout = {title, load, reps};
 
-        const response = await fetch('/api/workouts', {
+        const response = await fetch(
+            `${process.env.REACT_APP_API_URL}/api/workouts`, 
+            {
             method: "POST",
             body: JSON.stringify(workout),
             headers: {
